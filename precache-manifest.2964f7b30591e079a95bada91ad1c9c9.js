3737352fcb2d0fe4445e18ab42e25ba4af2760b8
Anthony Zhou <anthonyfzhou@berkeley.edu>
2020-10-23 15:08:26 -0700

    Updates

---
+++ precache-manifest.2964f7b30591e079a95bada91ad1c9c9.js
@@ -0,0 +1,526 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c76e452b9afab49232ae107d6fafaa1",
+    "url": "/codeology_website/index.html"
+  },
+  {
+    "revision": "6c52d45d1b59a9ea4db6",
+    "url": "/codeology_website/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "2337913c00f54a7cc90a",
+    "url": "/codeology_website/static/css/main.fa730f6b.chunk.css"
+  },
+  {
+    "revision": "6c52d45d1b59a9ea4db6",
+    "url": "/codeology_website/static/js/2.0574c8cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/codeology_website/static/js/2.0574c8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2337913c00f54a7cc90a",
+    "url": "/codeology_website/static/js/main.0aa63424.chunk.js"
+  },
+  {
+    "revision": "884b18310398238b87bc",
+    "url": "/codeology_website/static/js/runtime-main.b55ba8c7.js"
+  },
+  {
+    "revision": "8d6f6ff0bcc4512f99f05758b51590ec",
+    "url": "/codeology_website/static/media/GitHub-Mark.8d6f6ff0.eps"
+  },
+  {
+    "revision": "c673e59a3d4b431e5c61f12ab3beba5f",
+    "url": "/codeology_website/static/media/GitHub-Mark.c673e59a.zip"
+  },
+  {
+    "revision": "d26611a8033adc15cdd0bb7837e6d5bb",
+    "url": "/codeology_website/static/media/GitHub-Mark.d26611a8.ai"
+  },
+  {
+    "revision": "301b635be19137289bdc75c4c16cb403",
+    "url": "/codeology_website/static/media/Wall.301b635b.png"
+  },
+  {
+    "revision": "612155da7cf2793cc6b51bdd1eb12686",
+    "url": "/codeology_website/static/media/aarushi.612155da.jpg"
+  },
+  {
+    "revision": "8031dc5c7aca30a6f6d54613ce0863e0",
+    "url": "/codeology_website/static/media/abinav.8031dc5c.jpg"
+  },
+  {
+    "revision": "f50a4f13a5ef998b898096b0392b0e59",
+    "url": "/codeology_website/static/media/ada.f50a4f13.jpg"
+  },
+  {
+    "revision": "b23e49cc50927a4c4adbe551402362a9",
+    "url": "/codeology_website/static/media/adithya.b23e49cc.jpg"
+  },
+  {
+    "revision": "18d0691bdf7e8ab0c2f6ceb69030faea",
+    "url": "/codeology_website/static/media/ahmed.18d0691b.jpg"
+  },
+  {
+    "revision": "64f98453f4d4a971be16e82bf747dccd",
+    "url": "/codeology_website/static/media/ali.64f98453.jpg"
+  },
+  {
+    "revision": "53b5b452b43edf85473e1e1283f13759",
+    "url": "/codeology_website/static/media/alma.53b5b452.jpg"
+  },
+  {
+    "revision": "37f36f256e120fb57e2091971ccfdc53",
+    "url": "/codeology_website/static/media/amy.37f36f25.jpg"
+  },
+  {
+    "revision": "bb1415a1eac2c2642998ba41c8f8d8f3",
+    "url": "/codeology_website/static/media/andrew.bb1415a1.jpg"
+  },
+  {
+    "revision": "8dd8d65860ea08c1705497ae1e5bbe23",
+    "url": "/codeology_website/static/media/angela1.8dd8d658.jpg"
+  },
+  {
+    "revision": "9a0e66460b0d91055c5cb36966072e27",
+    "url": "/codeology_website/static/media/angela2.9a0e6646.jpg"
+  },
+  {
+    "revision": "82c6a277aad4ee0f5eb17254788c9e70",
+    "url": "/codeology_website/static/media/anna.82c6a277.jpg"
+  },
+  {
+    "revision": "2df4de91e326351d79a612f1adc847f3",
+    "url": "/codeology_website/static/media/anthony.2df4de91.jpg"
+  },
+  {
+    "revision": "8b4a0d1054f4c2304a59fe981bd80c18",
+    "url": "/codeology_website/static/media/ashley.8b4a0d10.jpg"
+  },
+  {
+    "revision": "ef39647ce3fd9733489916f81651bcdf",
+    "url": "/codeology_website/static/media/ashleychu.ef39647c.jpg"
+  },
+  {
+    "revision": "d2e7000a8ec5d7f72568537cdbd67d1e",
+    "url": "/codeology_website/static/media/avocodeology.d2e7000a.jpg"
+  },
+  {
+    "revision": "254b5aa6ccc0f871ad45e2ba9bc3a61e",
+    "url": "/codeology_website/static/media/ayush.254b5aa6.jpg"
+  },
+  {
+    "revision": "ee436789c4a28d7502ba8673433132df",
+    "url": "/codeology_website/static/media/balling-with-data.ee436789.png"
+  },
+  {
+    "revision": "0819ffd0b3f9f04fb2165ccaa68faf2f",
+    "url": "/codeology_website/static/media/big_puzzle.0819ffd0.png"
+  },
+  {
+    "revision": "4044817697f1144c52638d27faa70514",
+    "url": "/codeology_website/static/media/big_puzzle2.40448176.png"
+  },
+  {
+    "revision": "e1b01d6389a8861654ccb9fcd03cb682",
+    "url": "/codeology_website/static/media/bridget.e1b01d63.jpg"
+  },
+  {
+    "revision": "5463ef1b63aaba552ed89fbc07e1e746",
+    "url": "/codeology_website/static/media/calvin.5463ef1b.jpg"
+  },
+  {
+    "revision": "963bd1aba63de0aa15a1ceb4df2b8c41",
+    "url": "/codeology_website/static/media/charlize.963bd1ab.jpg"
+  },
+  {
+    "revision": "48aabb495d9b6f07eccdb259f5e2e1be",
+    "url": "/codeology_website/static/media/chatbot.48aabb49.jpg"
+  },
+  {
+    "revision": "58bdd86f90c0533fbe669318a32c635d",
+    "url": "/codeology_website/static/media/chicken.58bdd86f.jpg"
+  },
+  {
+    "revision": "27111fb91dc36c8537e01c2ed0638623",
+    "url": "/codeology_website/static/media/christine.27111fb9.jpg"
+  },
+  {
+    "revision": "b3a6d3c0473f449998edb84d9ef88f64",
+    "url": "/codeology_website/static/media/cindy.b3a6d3c0.jpg"
+  },
+  {
+    "revision": "10152a0562f229e3caf6912e836d3d60",
+    "url": "/codeology_website/static/media/cj.10152a05.jpg"
+  },
+  {
+    "revision": "72a85bb34f81c60bf765cf33834b6eb1",
+    "url": "/codeology_website/static/media/clark.72a85bb3.jpg"
+  },
+  {
+    "revision": "4535045e8c5a2e5e00e63c89ef32e642",
+    "url": "/codeology_website/static/media/club.4535045e.jpg"
+  },
+  {
+    "revision": "5a7ad8767d1c6b03f4a5e439f9cc329f",
+    "url": "/codeology_website/static/media/daniel.5a7ad876.jpg"
+  },
+  {
+    "revision": "c89c3121a08bdcafbba94f943cc53926",
+    "url": "/codeology_website/static/media/david.c89c3121.jpg"
+  },
+  {
+    "revision": "3edcba7e072596b15e13b6dc11c6dff5",
+    "url": "/codeology_website/static/media/discord-bot.3edcba7e.jpg"
+  },
+  {
+    "revision": "08358e8670a95c849fbf8b3fb58b876e",
+    "url": "/codeology_website/static/media/elizabeth.08358e86.jpg"
+  },
+  {
+    "revision": "cd00d661388ebca03feaa2193c34ff96",
+    "url": "/codeology_website/static/media/emily1.cd00d661.jpg"
+  },
+  {
+    "revision": "be3c9f104f3f07de026eb6434c302470",
+    "url": "/codeology_website/static/media/emily2.be3c9f10.jpg"
+  },
+  {
+    "revision": "b984bd69d14970366235043131f52cbf",
+    "url": "/codeology_website/static/media/eric.b984bd69.jpg"
+  },
+  {
+    "revision": "ea8d81c37e91068425d7d7ba7d7e2af4",
+    "url": "/codeology_website/static/media/ethan.ea8d81c3.jpg"
+  },
+  {
+    "revision": "d68b654692b049d4539a87474bb281c0",
+    "url": "/codeology_website/static/media/film.d68b6546.jpg"
+  },
+  {
+    "revision": "19c41b4907e230323860543566040730",
+    "url": "/codeology_website/static/media/gentrification.19c41b49.jpg"
+  },
+  {
+    "revision": "a55314b71178b3440577060c9407d956",
+    "url": "/codeology_website/static/media/grace.a55314b7.jpg"
+  },
+  {
+    "revision": "478e335312d7bd5a7eaaf5c8669c10a6",
+    "url": "/codeology_website/static/media/howie.478e3353.jpg"
+  },
+  {
+    "revision": "76af350ac6c0aa0a96cad722bacfe746",
+    "url": "/codeology_website/static/media/jag.76af350a.jpg"
+  },
+  {
+    "revision": "b44be76da198c69b5e440e1c9c858864",
+    "url": "/codeology_website/static/media/jaj.b44be76d.jpg"
+  },
+  {
+    "revision": "b344006c98455b77e925728aae25b574",
+    "url": "/codeology_website/static/media/jasper.b344006c.jpg"
+  },
+  {
+    "revision": "93a33677ca2d371b205d1597698e81b6",
+    "url": "/codeology_website/static/media/jennifer1.93a33677.jpg"
+  },
+  {
+    "revision": "c4aae082e6e41f6f9d080c1344def818",
+    "url": "/codeology_website/static/media/jennifer2.c4aae082.jpg"
+  },
+  {
+    "revision": "0b1993d108529c98af2cf59a580df178",
+    "url": "/codeology_website/static/media/jess.0b1993d1.jpg"
+  },
+  {
+    "revision": "59023dfcd3c4de7a6d6ee1ea75e939f5",
+    "url": "/codeology_website/static/media/jimmy.59023dfc.jpg"
+  },
+  {
+    "revision": "ab554db86e3076da46d562291ebfbed7",
+    "url": "/codeology_website/static/media/jimmy2.ab554db8.jpg"
+  },
+  {
+    "revision": "00eb834dfbe7b660dde1d3388a4bc128",
+    "url": "/codeology_website/static/media/jinsu.00eb834d.jpg"
+  },
+  {
+    "revision": "73a59747938b5c8dc5f4221b8dd21bce",
+    "url": "/codeology_website/static/media/kaavya.73a59747.jpg"
+  },
+  {
+    "revision": "2f1e6f3b4813af747600f131900dd2b5",
+    "url": "/codeology_website/static/media/kamille.2f1e6f3b.jpg"
+  },
+  {
+    "revision": "20aa27ef990c25b3364e582590a4a0d1",
+    "url": "/codeology_website/static/media/karina.20aa27ef.jpg"
+  },
+  {
+    "revision": "b82aeb0b7c8851edebe6897bf6a0ee7a",
+    "url": "/codeology_website/static/media/karthick.b82aeb0b.jpg"
+  },
+  {
+    "revision": "22018a61e13fd76a2ab8078641171547",
+    "url": "/codeology_website/static/media/katherine.22018a61.jpg"
+  },
+  {
+    "revision": "feca9d5ec8e78cc41b09e7f3c7692621",
+    "url": "/codeology_website/static/media/kevin.feca9d5e.jpg"
+  },
+  {
+    "revision": "efd118fccc0bb8e70fc2f4264337c215",
+    "url": "/codeology_website/static/media/kevinchai.efd118fc.jpg"
+  },
+  {
+    "revision": "76ac50546033acf1ef12897ad7b4f778",
+    "url": "/codeology_website/static/media/krishna.76ac5054.jpg"
+  },
+  {
+    "revision": "aeb4907aec8f9a158854a31cb7ac7a3e",
+    "url": "/codeology_website/static/media/kristie.aeb4907a.jpg"
+  },
+  {
+    "revision": "611110f35a4c63bdfcbada0603a224c2",
+    "url": "/codeology_website/static/media/kyle.611110f3.jpg"
+  },
+  {
+    "revision": "eaf614f8d805016c5a83ea594f68d1bb",
+    "url": "/codeology_website/static/media/kyle.eaf614f8.jpg"
+  },
+  {
+    "revision": "2068afa5b5eaf053ab1a8a647d28f683",
+    "url": "/codeology_website/static/media/logo.2068afa5.png"
+  },
+  {
+    "revision": "5716e3112b4a48fa1cc30417f7502c03",
+    "url": "/codeology_website/static/media/lyric.5716e311.jpg"
+  },
+  {
+    "revision": "06d0a003d7b15ce666c346808dc7cc02",
+    "url": "/codeology_website/static/media/mantej.06d0a003.jpg"
+  },
+  {
+    "revision": "90960639ca49886d9ea934326b4f1bf4",
+    "url": "/codeology_website/static/media/marvin.90960639.jpg"
+  },
+  {
+    "revision": "23fb2807f5f185a24297a6a5496204a4",
+    "url": "/codeology_website/static/media/matt.23fb2807.jpg"
+  },
+  {
+    "revision": "661a48e8b9bc0a0522c6e55f317b4322",
+    "url": "/codeology_website/static/media/micah.661a48e8.jpg"
+  },
+  {
+    "revision": "a1e3c03ac30b89ff4d2338cc85415ecf",
+    "url": "/codeology_website/static/media/michael.a1e3c03a.jpg"
+  },
+  {
+    "revision": "f98023250e99d8b73ae33d4844b300c7",
+    "url": "/codeology_website/static/media/michelle.f9802325.jpg"
+  },
+  {
+    "revision": "5dadca4e545bcbe706cf772657daa78e",
+    "url": "/codeology_website/static/media/nikita.5dadca4e.jpg"
+  },
+  {
+    "revision": "7694ae7b0b6871a37a80780f1b7bb924",
+    "url": "/codeology_website/static/media/nitzan.7694ae7b.jpg"
+  },
+  {
+    "revision": "e4f376be8705f9e95cfacb6a5312f9d9",
+    "url": "/codeology_website/static/media/omar.e4f376be.jpg"
+  },
+  {
+    "revision": "4b0ddfd8951abce0a8b4f031747c986b",
+    "url": "/codeology_website/static/media/person1.4b0ddfd8.png"
+  },
+  {
+    "revision": "8c2a2066b3f9a88acd016139fb06e509",
+    "url": "/codeology_website/static/media/person2.8c2a2066.png"
+  },
+  {
+    "revision": "7ca050eff31dc16a9e568e41f1095666",
+    "url": "/codeology_website/static/media/person3.7ca050ef.png"
+  },
+  {
+    "revision": "4f2b00cecd85ce5dcd8a86210965a38e",
+    "url": "/codeology_website/static/media/person4.4f2b00ce.png"
+  },
+  {
+    "revision": "4e62f5543a764ecfb39a751748a21dc7",
+    "url": "/codeology_website/static/media/personal-website.4e62f554.jpg"
+  },
+  {
+    "revision": "f4482da2128d7d422c0af676bffbe0f9",
+    "url": "/codeology_website/static/media/pink arm.f4482da2.png"
+  },
+  {
+    "revision": "8bc9837055248e2232154908022eceb4",
+    "url": "/codeology_website/static/media/predicting-film-success.8bc98370.jpg"
+  },
+  {
+    "revision": "69d7b3f7a2f69f37bebd43f54c85517c",
+    "url": "/codeology_website/static/media/projectsRec.69d7b3f7.png"
+  },
+  {
+    "revision": "647c6d20141ed2750b28bc57bfb69999",
+    "url": "/codeology_website/static/media/puzzle.647c6d20.png"
+  },
+  {
+    "revision": "6b97eeb49f260bd880d515f55b05739b",
+    "url": "/codeology_website/static/media/red arm.6b97eeb4.png"
+  },
+  {
+    "revision": "ea39cdb6870b4d15617bc2c63e601be7",
+    "url": "/codeology_website/static/media/richard.ea39cdb6.jpg"
+  },
+  {
+    "revision": "dcd177ee8ddcd40f009b9bfa15fae14c",
+    "url": "/codeology_website/static/media/roger.dcd177ee.jpg"
+  },
+  {
+    "revision": "0f6ccbf8003573a9b501cde5a3516b7e",
+    "url": "/codeology_website/static/media/roshan.0f6ccbf8.jpg"
+  },
+  {
+    "revision": "c6abcf7b2677d12c25c853e575f425f4",
+    "url": "/codeology_website/static/media/sai.c6abcf7b.jpg"
+  },
+  {
+    "revision": "6de910cb1b00f0527ceeddfbc8ea359c",
+    "url": "/codeology_website/static/media/saketh.6de910cb.jpg"
+  },
+  {
+    "revision": "7084cac1837374e59a546a1cc2941ba7",
+    "url": "/codeology_website/static/media/sample.7084cac1.png"
+  },
+  {
+    "revision": "b22e3c0497b6c639c2a98ba20d278cfd",
+    "url": "/codeology_website/static/media/satiya.b22e3c04.jpg"
+  },
+  {
+    "revision": "f5ca4f4680d2dd7660404befa596aa58",
+    "url": "/codeology_website/static/media/seawarden.f5ca4f46.png"
+  },
+  {
+    "revision": "2b96f868866624afb891743d557cb144",
+    "url": "/codeology_website/static/media/sera.2b96f868.jpg"
+  },
+  {
+    "revision": "eadc72dc0c1a9870217342eed77eb33f",
+    "url": "/codeology_website/static/media/shaylan.eadc72dc.jpg"
+  },
+  {
+    "revision": "3847482d5f6abb21c402686fac055157",
+    "url": "/codeology_website/static/media/sitting_person.3847482d.png"
+  },
+  {
+    "revision": "af6a58b58164e56750e0a19773cdc1f8",
+    "url": "/codeology_website/static/media/small-monkey-brain-react.af6a58b5.jpeg"
+  },
+  {
+    "revision": "4b32a9ffbfcad8b6a149fb06e4508c81",
+    "url": "/codeology_website/static/media/small_puzzle.4b32a9ff.png"
+  },
+  {
+    "revision": "e71b6df0a1bb1580c6576bc00719b70d",
+    "url": "/codeology_website/static/media/smallpeeps left.e71b6df0.png"
+  },
+  {
+    "revision": "f459cab2240899c37b02e28650733b78",
+    "url": "/codeology_website/static/media/smallpeeps right.f459cab2.png"
+  },
+  {
+    "revision": "d5f3b2c0fc00a4ea81adf4258cd6aea1",
+    "url": "/codeology_website/static/media/sneh.d5f3b2c0.jpg"
+  },
+  {
+    "revision": "bed0df01f7717592b7357097e342a5f8",
+    "url": "/codeology_website/static/media/space.bed0df01.jpg"
+  },
+  {
+    "revision": "187a5aa0a58040b02e6bdfd3bbcf9360",
+    "url": "/codeology_website/static/media/srishti.187a5aa0.jpg"
+  },
+  {
+    "revision": "7548f59ddc869c8e2c3c00bdc97868e3",
+    "url": "/codeology_website/static/media/standing1.7548f59d.png"
+  },
+  {
+    "revision": "1873f4884e78c1521ecd58da4dcf4011",
+    "url": "/codeology_website/static/media/standing2.1873f488.png"
+  },
+  {
+    "revision": "ef77675f211edbd0a7fb98e2f06ddd40",
+    "url": "/codeology_website/static/media/stella.ef77675f.jpg"
+  },
+  {
+    "revision": "f47cfc24a281ef7b3bc54481ac52ae11",
+    "url": "/codeology_website/static/media/susan.f47cfc24.jpg"
+  },
+  {
+    "revision": "da7d6ecd1a711317e49be8b43272dd49",
+    "url": "/codeology_website/static/media/swift-squad.da7d6ecd.png"
+  },
+  {
+    "revision": "12ac3ad1bebe1cb7564b600e47933e16",
+    "url": "/codeology_website/static/media/taylor.12ac3ad1.jpg"
+  },
+  {
+    "revision": "461ca52c676507e5eca13f4886896789",
+    "url": "/codeology_website/static/media/tiffany.461ca52c.jpg"
+  },
+  {
+    "revision": "9442cfd20c9eb22ce8abef6b533408d1",
+    "url": "/codeology_website/static/media/tina.9442cfd2.jpg"
+  },
+  {
+    "revision": "173ff5ea3b95dedf8ed60fd551489fb3",
+    "url": "/codeology_website/static/media/trevor.173ff5ea.jpg"
+  },
+  {
+    "revision": "4889f5f1129f26ff731edea574ab8647",
+    "url": "/codeology_website/static/media/unity-rpg.4889f5f1.png"
+  },
+  {
+    "revision": "2296b7d5297487d5c730c157bb2ee9bd",
+    "url": "/codeology_website/static/media/vaibhav.2296b7d5.jpg"
+  },
+  {
+    "revision": "f76a7cf1d8ab704e281669bcc8a6743e",
+    "url": "/codeology_website/static/media/varun.f76a7cf1.jpg"
+  },
+  {
+    "revision": "767e99aaa92c96fed0ba8afdfd66e909",
+    "url": "/codeology_website/static/media/vidya.767e99aa.jpg"
+  },
+  {
+    "revision": "cdf9a653dcdbf994d3283bca9be35929",
+    "url": "/codeology_website/static/media/vscodeology.cdf9a653.png"
+  },
+  {
+    "revision": "c11d7b54262c9ce208b65b527fd88c79",
+    "url": "/codeology_website/static/media/web_hand1.c11d7b54.png"
+  },
+  {
+    "revision": "106a9ac1736d36a3f0018c9ed9df494c",
+    "url": "/codeology_website/static/media/web_hand2.106a9ac1.png"
+  },
+  {
+    "revision": "3a387c79f37565e4eb6a4a0adb86d7a5",
+    "url": "/codeology_website/static/media/web_hand3.3a387c79.png"
+  },
+  {
+    "revision": "280628556e9e2d5582a1d0e386f982ca",
+    "url": "/codeology_website/static/media/web_hand4.28062855.png"
+  },
+  {
+    "revision": "b7fa3d3debef680723510ff93559c66a",
+    "url": "/codeology_website/static/media/zoom.b7fa3d3d.jpg"
+  }
+]);
